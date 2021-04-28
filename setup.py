e5cbb296143b936bcdac3c4b5c8a3ceca3f32849
Andrew White <white.d.andrew@gmail.com>
2021-04-27 21:35:54 -0400

    Added example 1 and made package installable (#1)

---
+++ setup.py
@@ -0,0 +1,42 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="maxent",
+    version="1.0",
+    url="https://github.com/ur-whitelab/maxent",
+    license='MIT',
+
+    author="Rainier Barret, Mehrad Ansari, Andrew D White",
+    author_email="andrew.white@rochester.edu",
+
+    description="Maximum entropy inference Keras implementation",
+    long_description=read("README.md"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=['numpy', 'matplotlib', 'tensorflow', 'tqdm'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
