1e412ba2421ebc9712af1cfc84bc445909b11314
Andrew White <white.d.andrew@gmail.com>
2021-08-13 16:43:16 -0400

    Added type hints to source

--- maxent/__init__.py
+++ maxent/__init__.py
@@ -1,9 +1,18 @@
 """maxent - Maximum Entropy"""
 
-__version__ = '0.1'
-__author__ = 'Mehrad Ansari <Mehrad.ansari@rochester.edu>, Rainier Barrett <rainier.barrett@gmail.com>, Andrew White <andrew.white@rochester.edu>'
+__version__ = "0.2"
+__author__ = "Mehrad Ansari <Mehrad.ansari@rochester.edu>, Rainier Barrett <rainier.barrett@gmail.com>, Andrew White <andrew.white@rochester.edu>"
 __all__ = []
 
 from .hyper import ParameterJoint, TrainableInputLayer, HyperMaxentModel
-from .core import Prior, EmptyPrior, Laplace, MaxentModel, Restraint, \
-    AvgLayerLaplace, AvgLayer, ReweightLayer, ReweightLayerLaplace
+from .core import (
+    Prior,
+    EmptyPrior,
+    Laplace,
+    MaxentModel,
+    Restraint,
+    AvgLayerLaplace,
+    AvgLayer,
+    ReweightLayer,
+    ReweightLayerLaplace,
+)
--- setup.py
+++ setup.py
@@ -8,36 +8,30 @@ from setuptools import setup
 
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u'')
-    with io.open(filename, mode='r', encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
 
 
 setup(
-    name='maxent',
-    version='1.0',
-    url='https://github.com/ur-whitelab/maxent',
-    license='GPL v2',
-
-    author='Rainier Barret, Mehrad Ansari, Andrew D White',
-    author_email='andrew.white@rochester.edu',
-
-    description='Maximum entropy inference Keras implementation',
-    long_description=read('README.md'),
-
-    packages=find_packages(exclude=('tests',)),
-
-    install_requires=['numpy',
-                      'tensorflow', 'tensorflow_probability'],
-
+    name="maxent",
+    version="1.0",
+    url="https://github.com/ur-whitelab/maxent",
+    license="GPL v2",
+    author="Rainier Barret, Mehrad Ansari, Andrew D White",
+    author_email="andrew.white@rochester.edu",
+    description="Maximum entropy inference Keras implementation",
+    long_description=read("README.md"),
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["numpy", "tensorflow", "tensorflow_probability"],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: GPL v2 License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GPL v2 License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
